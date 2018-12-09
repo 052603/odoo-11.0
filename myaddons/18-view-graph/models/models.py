b5a7374b7d906189a1a2df5c8bb335997d2c2b3e
18428 <jaxyan@126.com>
2018-12-09 04:32:40 -0800

    18-view-graph

---
+++ myaddons/18-view-graph/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class electric_meter(models.Model):
+    _name = 'electric.meter'
+
+    name = fields.Char()
+    elec_id = fields.Integer(string='编号')
+    elec_current = fields.Float(default=0, string='电流')
+    elec_voltage = fields.Float(default=0, string='电压')
+    elec_electricity = fields.Float(default=0, string='电量')
+    elec_electricity_demo = fields.Float(default=0, string='电量demo')
+    elec_date = fields.Datetime()
+    elec_str = fields.Char(string='日期')
+
+
